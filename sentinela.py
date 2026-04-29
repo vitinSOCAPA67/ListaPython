2101c5d9b1d356ca49985a8c456122473b94d5c0
VictorCardoso740 <victorhugocardoso740@gmail.com>
2026-04-29 14:04:28 +0000

    v.1

--- sentinela.py
+++ sentinela.py
@@ -1,1 +1,6 @@
-
+#--Variável Sentinela--
+while True:
+    comando = input("Digite o Comando, Se Precisar Parar Digige: Sair")
+    if comando == "sair":
+        break
+    print(f"executando:{comando}")
