30f40215e4913b6abc855b83753d0f859dde94db
VictorCardoso740 <victorhugocardoso740@gmail.com>
2026-05-04 07:52:00 -0300

    Update seletor.py

--- seletor.py
+++ seletor.py
@@ -1,1 +1,8 @@
-
+#Sletor a Caracteres--
+letra = input("Digite uma Letra: ").upper()
+if letra = 'A':
+  print("Você Selecionou a Opcão A")
+elif letra == 'B':
+  print("Você Selecionou a Opção B")
+else:
+  print("Essa Opção Não está Disponivel")
