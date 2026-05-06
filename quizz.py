604e3b7fa21cf0f5e57b3e1154451173a6567f69
VictorCardoso740 <victorhugocardoso740@gmail.com>
2026-05-06 23:27:52 +0000

    v5

--- quizz.py
+++ quizz.py
@@ -1,17 +1,22 @@
 #Quiz--
 print("Responda as Perguntas Abaixo Corretamente: ")
-print("Quantas Bolas de Ouro Tem Cristiano Ronaldo? \n A: 3 \n B: 2 \n C: 5").upper()
-resposta_1 = input("Digite Sua Resposta: ")
+print("1- Quantas Bolas de Ouro Tem Cristiano Ronaldo? \n A: 3 \n B: 2 \n C: 5")
+resposta_1 = input("Digite Sua Resposta: ").upper()
 if resposta_1 == "C":
     print("Resposta Correta!")
 else:
     print("Resposta Incorreta!")
 print("A Seguir, Questão 2:")
-print("Em Que Ano o Brasil Foi Descoberto? \n A: 1500 \n B:1460 \n C: 1325").upper()
-resposta_2 = input("Digite a Sua Resposta: ")
+print("2- Em Que Ano o Brasil Foi Descoberto? \n A: 1500 \n B: 1460 \n C: 1325")
+resposta_2 = input("Digite a Sua Resposta: ").upper()
 if resposta_2 == "A":
     print("Resposta Correta!")
 else: 
     print("Resposta Incorreta!")
 print("A seguir, a Questão 3: ")
-print("3: Quantos Planetas Existem No Sistema Solar? \n A:5 \n B:8 \n C:9")
+print("3- Quantos Planetas Existem No Sistema Solar? \n A:5 \n B:8 \n C:9")
+resposta_3 = input("Digite Sua Resposta: ").upper()
+if resposta_3 == "B":
+    print("Resposta Correta!")
+else:
+    print("Resposta Incorreta!")
