d5352b30348825685509fd6a949eed2836ee80f8
VictorCardoso740 <victorhugocardoso740@gmail.com>
2026-05-18 10:51:10 +0000

    tabuad

--- tabuada.py
+++ tabuada.py
@@ -1,1 +1,6 @@
-
+print("Tabuada Digital")
+numero = int(input("Qual Número Você Quer Saber a Tabuada? "))
+print(f"---Tabuada Do {numero}---")
+for i in range(1, 11):
+    resultado = numero * i 
+    print(f"{numero} X {i} = {resultado}")
