bfd53ece780549ede25a215787704e8ed87adbe3
VictorCardoso740 <victorhugocardoso740@gmail.com>
2026-05-26 23:57:41 +0000

    V2

---
+++ simulador_investimento_cdb.py
@@ -0,0 +1,13 @@
+#Investimento CDB--
+print("Bem-Vindo(a) ao Centro de Investimento CDB")
+aporte = float(input("Quanto Você Vai Depositar Por Mês? "))
+juros = float(input("Qual é a Taxa de Juros Atual da Poupança? "))
+meses = int(input("Por Quantos Meses Você Vai Investir? "))
+juros_decimal = juros/100
+total = 0
+for mes in range (1, meses +1):
+    total = total + aporte
+    total = total + (total * juros_decimal)
+    print(f"Mês {mes}: Saldo Total = R${total:.2f}")
+print(f"Ao Final de {meses} meses, Você Terá o Valor de R$:{total:.2f}")
+#Victor Hugo Cardoso 2EDS--
