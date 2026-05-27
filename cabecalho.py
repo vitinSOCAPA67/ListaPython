4fd9ba1f2199f75a6cbfd63ed759b820275099bf
VictorCardoso740 <victorhugocardoso740@gmail.com>
2026-05-27 13:16:29 +0000

    v1

---
+++ cabecalho.py
@@ -0,0 +1,9 @@
+### Definindo e Chamando a Função
+def exibir_cabecalho():
+    print("=" * 30)
+    print("SISTEMA DE GESTÃO ESCOLAR")
+    print("Versão 1.0 - Desenvolvido no Codespace")
+    print("=" * 30)
+    ### Chamada a Função
+    exibir_cabecalho()
+    print("Menu Principal: ")
--- simulador_investimento_cdb.py
+++ simulador_investimento_cdb.py
@@ -1,14 +1,13 @@
 #Investimento CDB--
 print("Bem-Vindo(a) ao Centro de Investimento CDB")
-aporte = float(input("Quanto Você Vai Depositar Por Mês? "))
-juros = 1.24
-meses = int(input("Por Quantos Meses Você Vai Investir? "))
 print("Taxa de Juros Atual do CDB: 1,24%")
-juros_decimal = juros/100
+aporte = float(input("Quanto Você Vai Depositar Por Mês? "))
+juros = 0.0124
+meses = int(input("Por Quantos Meses Você Vai Investir? "))
 total = 0
 for mes in range (1, meses +1):
     total = total + aporte
-    total = total + (total * juros_decimal)
+    total = total + (total * juros)
     print(f"Mês {mes}: Saldo Total = R${total:.2f}")
 print(f"Ao Final de {meses} meses, Você Terá o Valor de R$:{total:.2f}")
 #Victor Hugo Cardoso 2EDS--
