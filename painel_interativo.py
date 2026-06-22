c824820153cbcef2ebe550b0bc2b278e7f4fa9c8
VictorCardoso740 <victorhugocardoso740@gmail.com>
2026-06-22 11:32:56 +0000

    v2

---
+++ painel_interativo.py
@@ -0,0 +1,6 @@
+fila_espera = ["Senha 01", "Senha 02", "Senha 03", "Senha 04"]
+senha_atual = 0
+while senha_atual < len(fila_espera):
+    print("\n ==================")
+    print(f"Senha Atual: {fila_espera[atual - 1]}")
+    if senha
