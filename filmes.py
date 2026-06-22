b6685e82a307b90b4c411d5aeb85c5073787319c
VictorCardoso740 <victorhugocardoso740@gmail.com>
2026-06-22 10:56:29 +0000

    v1

---
+++ filmes.py
@@ -0,0 +1,4 @@
+filmes = ["O Farmador", "Star Skibidi", "Aura-men"]
+print(filmes[0])
+print(filmes[1])
+print(filmes[2])
---
+++ painel.py
@@ -0,0 +1,6 @@
+print("--- Painel de Atendimento ---")
+fila_espera = ["Senha 01", "Senha 02", "Senha 03", "Senha 04"]
+total = len(fila_espera)
+print(f"O Proximo Paciente a Ser Atendido é: {fila_espera[0]}:")
+print(f"O Segundo Paciente a Ser Atendido é: {fila_espera[1]}:")
+print(f"Total Aguardando: {total}")
